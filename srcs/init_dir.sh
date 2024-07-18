2570e4a07dab7b56ba1d81228efff6ceb224b9cf
seongwol <gbssw13@gmail.com>
2024-07-18 17:57:08 +0900

    myan

--- srcs/init_dir.sh
+++ srcs/init_dir.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-if [ "$1" == "--delete" ]; then
-    sed -i.bak '/DATA_PATH=/d' srcs/.env && rm srcs/.env.bak
-    echo "Removed DATA_PATH from srcs/.env"
-
-    rm -rf "$BASE_DIR/wordpress/data"
-    rm -rf "$BASE_DIR/mariadb/data"
-    echo "Removed data directories"
-
-    exit 0
-fi
-
 if [ "$(uname)" == "Darwin" ]; then
     # macOS
     BASE_DIR="/Users/$USER/Documents/volume"
 else
     # Linux
     BASE_DIR="/home/$USER/data"
+fi
+
+if [ "$1" == "--delete" ]; then
+    sed -i.bak '/DATA_PATH=/d' srcs/.env && rm srcs/.env.bak
+    rm -rf "$BASE_DIR"
+    rm -rf "$BASE_DIR"
+    exit 0
 fi
 
 if [ ! -d "/Users/seongwol/Documents/volume/wordpress/data" ]; then
