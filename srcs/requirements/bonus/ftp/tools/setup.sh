cd684dfb92c932ba0eadb39464df98338ffc1a4e
seongwol <gbssw13@gmail.com>
2024-07-19 02:19:34 +0900

    asdf

--- srcs/requirements/bonus/ftp/tools/setup.sh
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-adduser -D ${FTP_USER}
+adduser -D ${FTP_USER}  
 
+chown -R ${FTP_USER}:${FTP_USER} /var/www/html
+chmod -R 755 /var/www/html
 echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 
 echo "${FTP_USER}" >> /etc/vsftpd.userlist
